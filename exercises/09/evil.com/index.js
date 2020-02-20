377f52dbfac329ed4d589f37293f8c22ef77ebe7
Cassie Koomjian <cassie@substantial.com>
2020-02-20 15:46:01 -0800

    Add hints for exercise 9

--- exercises/09/evil.com/index.js
+++ exercises/09/evil.com/index.js
@@ -4,4 +4,12 @@ const app = localHost("evil.com");
 
 app.use(express.static(__dirname + "/static"));
 
+// 🐨 Add /hijack endpoint to log the payload query parameter
+// 💰 Here you go:
+// app.route("/hijack").get((request, response) => {
+//   console.log("received payload", request.query.payload);
+//   response.status(200);
+//   response.send("ok");
+// });
+
 app.listen(666);
